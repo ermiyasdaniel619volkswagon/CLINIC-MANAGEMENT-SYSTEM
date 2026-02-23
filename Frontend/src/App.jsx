21edb56194a59e4a2530999cfb2b47e9ff73cea2
Ermiyas Daniel <ermiyasdaniel619@gmail.com>
2026-02-23 06:27:43 -0800

    Initial React + Vite + Tailwind setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="text-[50px]">
+        integreted tallwind to react vite project successfully
+      </p>
+    </>
+  );
+}
+
+export default App;
